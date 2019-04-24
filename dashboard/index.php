40efb41e6d5c8b5a54374224dd7773064eb5950f
unknown <fredrik-hulaas@hotmail.com>
2019-04-24 17:38:43 +0200

    dashboard responsiv

--- dashboard/index.php
+++ dashboard/index.php
@@ -32,13 +32,40 @@ if(isset($_COOKIE['message'])) {
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
-    <link rel="stylesheet" type="text/css" href="../css/css.css">
+<!--    <link rel="stylesheet" media="screen and (min-width: 1051px)" href="../css/dashboard1920Css.css">-->
+    <link rel="stylesheet"  href="../css/dashboardDefaultCss.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
+    <!-- sidebar -->
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+
+    <style>
+        @media screen and (min-width: 1051px) and (max-width: 1920px) {
+            #framediv .container {
+                height: 450px;
+                width: 100%;
+            }
+
+            .dropdown {
+                position: relative;
+                display: inline-block;
+                padding-top: 12px;
+            }
 
 
+            .sidenav {
+                z-index: 1; /* Stay on top */
+                overflow-y: hidden; /* Disable horizontal scroll */
+                transition: 0.5s; /* 0.5 second transition effect to slide in the sidenav */
+                background-image: url("../images/betong.png");
+                height: 50px;
+                font-size: 25px;
+            }
+
+
+    </style>
 
 </head>
 <body>
@@ -48,25 +75,19 @@ if(isset($_COOKIE['message'])) {
 
         <a class="navbar-brand" href="#">  <img src="../images/logo.svg" alt="Ringerike kommune" title="Hjem" style="width:300px;height:50px;"></a>
 
-
-
-
-
-        <!-- Her er dropdown funksjonen som ligger oppe i høyre hjørnat -->
         <div class="dropdown" style="float: right;">
-            <i class="fa fa-cog" aria-hidden="true" style="font-size: 35px;">
-
+            <i class="fa fa-cog" aria-hidden="true" onclick="openRightMenu()" style="font-size: 35px;">
             </i>
-
-            <div class="dropdown-content">
-                <?php echo $_COOKIE['user_name'];?>
-                <a href="#">Bruker</a>
-                <a href="#">Informasjon</a>
-                <a href="#">Endre passord</a>
-                <a href="../include_login/logout.php"><span  class="glyphicon glyphicon-log-in" title="Logg ut" style="color: black;"></span> Logg ut </a>
-            </div>
         </div>
 
+        <div class="w3-sidebar w3-bar-block w3-card w3-animate-right" style="display:none; right:0;" id="rightMenu">
+            <button onclick="closeRightMenu()" class="w3-bar-item w3-button w3-large">Close &times;</button>
+            <p>Innlogget bruker: <br>  <?php echo $_COOKIE['user_name'];?></p>
+            <a href="#" class="w3-bar-item w3-button">Bruker</a>
+            <a href="#" class="w3-bar-item w3-button">Informasjon</a>
+            <a href="#" class="w3-bar-item w3-button">Endre passord</a>
+            <a href="../include_login/logout.php" class="w3-bar-item w3-button"><span  class="glyphicon glyphicon-log-in" title="Logg ut" style="color: black;"></span> Logg ut </a>
+        </div>
 
 
     </div>
@@ -75,27 +96,37 @@ if(isset($_COOKIE['message'])) {
 <!-- Venstre side -->
 <div class="sidenav">
 
-            <a><i class="fa fa-calendar" aria-hidden="true"><i style="display: none;">Kalender</i></i></a>
-            <a><i class="fa fa-building-o" aria-hidden="true"><i style="display: none;">Min arbeidsplan</i></i></a>
-            <a><i class="fa fa-commenting-o" aria-hidden="true"><i style="display: none;">Chat</i></i></a>
-    <a href="../include_login/logout.php"><span  class="glyphicon glyphicon-log-in" title="Logg ut" style="color: black;"></span> Logg ut </a>
-
+    <div class="dropdown-sidenav">
+        <a><i class="fa fa-calendar" aria-hidden="true"><i style="display: none;">Kalender</i></i></a>
+        <a><i class="fa fa-building-o" aria-hidden="true"><i style="display: none;">Min arbeidsplan</i></i></a>
+        <a><i class="fa fa-commenting-o" aria-hidden="true"><i style="display: none;">Chat</i></i></a>
+        <div class="dropdown-sidenav-content">
+            <a href="#">Kalender</a>
+            <a href="#">Arbeidsplan</a>
+            <a href="#">Chat</a>
+        </div>
+    </div>
 </div>
 
-
-
-
-
-
-<footer class="container-fluid text-center">
-    <p>Footer Text</p>
-</footer>
-
-
+<div id="framediv" class="container">
+    <iframe id="frame" src="../oversikt/oversikt.php"></iframe>
+    <footer class="container-fluid text-center">
+        <p>Footer Text</p>
+    </footer>
+</div>
 
 </body>
 
 <script>
+
+    function openRightMenu() {
+        document.getElementById("rightMenu").style.display = "block";
+    }
+
+    function closeRightMenu() {
+        document.getElementById("rightMenu").style.display = "none";
+    }
+
     $('#chatknapp').click(function () {
         $('#maincontent').load("../chat/chat.php");
         return false;
