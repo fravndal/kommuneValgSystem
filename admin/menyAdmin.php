4670b1b618d06a91ec5679b29b8d4566e0251b9c
fravndal <fredrik.ravndal@gmail.com>
2019-04-25 20:47:43 +0200

    Fikset forsiden for desktop

--- admin/menyAdmin.php
+++ admin/menyAdmin.php
@@ -4,6 +4,6 @@
     <a href="../oversikt/oversikt.php" target="targetFrame"><button id="btnTest" name="btnTest" type="button" class="btn btn-primary">Func1</button></a>
     <button type="button" class="btn btn-primary">Func2</button>
     <button type="button" class="btn btn-primary">Func3</button>
-    <button type="button" class="btn btn-primary">Func4</button>
+    <a href="applicationHandle.php" target="targetFrame"><button type="button" class="btn btn-primary">Håndter søknad</button></a>
     <button type="button" class="btn btn-primary">Func5</button>
 </div>
